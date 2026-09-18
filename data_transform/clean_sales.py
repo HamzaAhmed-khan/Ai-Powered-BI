4dae000998a98b891a170d5023236d0a78baf252
Hamza Ahmed Khan <152058128+HamzaAhmed-khan@users.noreply.github.com>
2026-09-18 19:11:17 +0500

    Add files via upload

---
+++ data_transform/clean_sales.py
@@ -0,0 +1,69 @@
+from data_transform.raw_data_fetcher import raw_data_fetcher
+from data_transform.raw_data_fetcher import engine, query
+import pandas as pd
+import numpy as np
+
+
+class CleanSales:
+    def __init__(self, raw_data_fetcher: pd.DataFrame):
+        self.raw_data = raw_data_fetcher
+
+    def clean_data(self) -> pd.DataFrame:
+        df = self.raw_data.copy()
+
+        # Rename columns to consistent naming
+        df.rename(
+            columns={
+                "date": "sale_date",
+                "age": "customer_age",
+                "total_amount": "sales_amount",
+            },
+            inplace=True,
+        )
+
+        # Drop duplicates
+        df.drop_duplicates(inplace=True)
+
+        # Handle missing values
+        df.fillna(
+            {
+                "customer_age": (
+                    df["customer_age"].median() if "customer_age" in df.columns else 0
+                ),
+                "gender": "Unknown",
+                "product": "Unknown",
+                "quantity": 0,
+                "price": 0.0,
+                "sales_amount": 0.0,
+            },
+            inplace=True,
+        )
+
+        # Convert data types
+        df["sale_date"] = pd.to_datetime(df["sale_date"], errors="coerce")
+        df["customer_age"] = pd.to_numeric(df["customer_age"], errors="coerce").astype(
+            int
+        )
+        df["quantity"] = pd.to_numeric(df["quantity"], errors="coerce").astype(int)
+        df["price"] = pd.to_numeric(df["price"], errors="coerce")
+        df["sales_amount"] = pd.to_numeric(df["sales_amount"], errors="coerce")
+
+        # Filter out invalid values
+        df = df[df["customer_age"].between(0, 120)]
+        df = df[df["sales_amount"] >= 0]
+        df = df[df["quantity"] >= 0]
+        df = df[df["price"] >= 0]
+
+        # Created a new column for money spent categorization
+
+        median_spent = df["sales_amount"].median()
+        df["spend_category"] = np.where(df["sales_amount"] > median_spent, "A", "B")
+        df["ingestion_timestamp"] = pd.Timestamp.now()
+        return df
+
+
+if __name__ == "__main__":
+    raw_data = raw_data_fetcher(query, engine)
+    cleaner = CleanSales(raw_data)
+    cleaned_data = cleaner.clean_data()
+    print(cleaned_data.head())
---
+++ data_transform/load_clean_data.py
@@ -0,0 +1,61 @@
+# Going to load data from bronze layer to silver layer
+import pandas as pd
+from data_transform.clean_sales import CleanSales
+from data_transform.raw_data_fetcher import engine, query, raw_data_fetcher
+from sqlalchemy import create_engine
+import os
+
+clean_db_host = os.getenv("CLEAN_DB_HOST", "localhost")
+clean_db_port = os.getenv("CLEAN_DB_PORT", "5434")
+db_user = os.getenv("POSTGRES_USER", "admin")
+db_password = os.getenv("POSTGRES_PASSWORD", "password123")
+clean_db = os.getenv("POSTGRES_DB_CLEAN", "bi_warehouse_clean")
+
+clean_db_url = (
+    f"postgresql://{db_user}:{db_password}@{clean_db_host}:{clean_db_port}/{clean_db}"
+)
+engine_clean = create_engine(clean_db_url)
+
+
+def extract_clean_data() -> pd.DataFrame:
+    # 1. Get current watermark from silver layer
+    try:
+        max_id_result = pd.read_sql(
+            "SELECT MAX(order_id) FROM clean.clean_sales_data", engine_clean
+        )
+        max_id = max_id_result.iloc[0, 0]
+        max_id = -1 if max_id is None else int(max_id)
+    except Exception:
+        max_id = -1
+
+    # 2. Extract only new data from raw layer
+    incremental_query = f"SELECT * FROM raw.sales_data WHERE order_id > {max_id}"
+    raw_data = pd.read_sql(incremental_query, engine.connect())
+
+    if raw_data.empty:
+        return pd.DataFrame()
+
+    cleaner = CleanSales(raw_data)
+    cleaned_data = cleaner.clean_data()
+    return cleaned_data
+
+
+def load_clean_data(clean_data):
+    if clean_data.empty:
+        print("No new data to load to silver layer.")
+        return
+
+    clean_data.to_sql(
+        name="clean_sales_data",
+        schema="clean",
+        con=engine_clean,
+        if_exists="append",
+        index=False,
+        method="multi",
+    )
+
+
+if __name__ == "__main__":
+    clean_data = extract_clean_data()
+    load_clean_data(clean_data)
+    print("Clean data loaded successfully!")
